604581ed67719d005a4220fc473d6b12bd7348b0
fehbarea <fehbarea@icloud.com>
2025-01-19 15:28:01 -0300

    estilizacao de botao

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,16 +1,32 @@
 import style from './LoginForm.module.css';
+import { useState } from 'react';
 
 function LoginForm() {
+
+    const [values, setValues] = useState({
+        user:'',
+        password:''
+    });
+
+    const handleChanges = (e) => { 
+        setValues({...values, [e.target.name]: e.target.value});
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(values);
+    }
+
     return (
         <div className={style.loginForm}>
             <img src='/images/Logotipo (Transparente) (1).png' alt='Logo do ELLP'/>
             <h2>Sistema de gerênciamento de voluntários e oficinas</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <section>
                     <label htmlFor='user'>Usuário</label>
-                    <input type='user' id='user' name='user' placeholder='Usuário' required />
+                    <input type='text' id='user' name='user' placeholder='Usuário' onChange={(e) => handleChanges(e)} required />
                     <label htmlFor='password'>Senha</label>
-                    <input type='password' id='password' name='password' placeholder='Senha' required />
+                    <input type='password' id='password' name='password' placeholder='Senha' onChange={(e) => handleChanges(e)} required />
                 </section>
                 
                 <button type='submit'>Entrar</button>

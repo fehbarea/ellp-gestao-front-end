f470ce8460bcf6eb6a5076d089d2e4469df45f6b
Marcelo Gregório Vieira <marcelovieira8967@gmail.com>
2025-02-12 10:50:29 -0300

    correção lista alunos

--- src/Services/alunosService.js
+++ src/Services/alunosService.js
@@ -1,24 +1,5 @@
 import api from './configAxios'
 
-export async function cadastrarVolunario(data) {
-
-    try {
-        await api.post('/voluntarios', data)
-
-    } catch (error) {
-        console.error('Erro ao fazer login:', error)
-        if (error.response) {
-            throw new Error(error.response.data.message || 'Erro ao fazer Cadastro')
-        }
-        else if (error.request) {
-            throw new Error('Sem resposta do servidor. Verifique sua conexão.')
-        }
-        else {
-            throw new Error('Erro');
-        }
-    }
-}
-
 export async function getAlunos() {
     try {
         const response = await api.get('/alunos');
